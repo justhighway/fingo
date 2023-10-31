56f830a5e6d45b44ca99dea2f8d186e156b913dd
justhighway <hirouth@naver.com>
2023-10-31 13:00:58 +0900

    로케이션 추가

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 import Home from "./screens/Home";
+import Weather from "./components/Weather";
 
 export default function App() {
-  return <Home />;
+  return <Weather />;
 }
